cd53d66abd0f300bcdc26fd7c67fff3c9676e96a
imasnabelliavenda <167957674+imasnabelliavenda@users.noreply.github.com>
2024-10-09 15:23:01 +0700

    Add files via upload

---
+++ Pertemuan 3 (tugas 1)/Pertemuan 4/Export Default/bola.js
@@ -0,0 +1,5 @@
+import {PHI} from '../Modules/math-constants.js';
+
+const volume = (radius) => 4 * PHI * radius ** 3;
+
+export default volume;
---
+++ Pertemuan 3 (tugas 1)/Pertemuan 4/Modules/math-constants.js
@@ -0,0 +1,2 @@
+export const PHI = 22/7;
+export const GOLDEN_RATIO = 1.61803398875;
